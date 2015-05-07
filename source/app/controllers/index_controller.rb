1dcf59416093272cec684944a1d7c3caae5d90be
Coderica <esm1018@gmail.com>
2015-05-07 16:57:44 -0700

    Finish MVC

--- source/app/controllers/index_controller.rb
+++ source/app/controllers/index_controller.rb
@@ -1,3 +1,12 @@
 get '/' do
 	erb :index
 end
+
+post '/short_answer' do
+	if request.xhr?
+		survey_question = params[:question]
+		return erb :_short_answer, :layout => false, locals: {question: survey_question}
+	else
+		return "hello"
+	end
+end
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
-	$('#new_question').hide();
-	$("#new_question_button").on('click', function(event){
+	$('#new_question_form').hide();
+	$('#create_survey_button').hide();
+	$("#create_new_question_button").on('click', function(event){
 		event.preventDefault();
-		$('#new_question').show();
-		$("#new_question_button").hide();
-		$("#create_survey_button").hide();
+		$('#new_question_form').show();
+		$("#create_new_question_button").hide();
+		$('#create_survey_button').show();
 	});
 
-	$("#new_question").on('submit', function(event){
+	$("#new_question_form").on('submit', function(event){
 		event.preventDefault();
-		$("#new_question").hide();
-		$("#create_survey_button").show();
-		$("#new_question_button").show();
-		// debugger
-		$('#question_list').append("<p>ajax</p>")
+		$("#new_question_form").hide();
+		$("#create_new_question_button").show();
+		var $surveyQuestion = $(this).find("input[type='text']")
+
+		var ajax = $.ajax({
+			url: '/short_answer',
+			type: 'POST',
+			data: {question: $surveyQuestion.val()}
+		});
+
+		ajax.done(function(response) {
+			$('#question_list').append(response)
+			$surveyQuestion.val("")
+		});
+
+		$("#question_list").on("click", "#delete_question_button", function(event) {
+			event.preventDefault();
+			var $question = $(this).closest(".partial_question")
+			debugger
+			$question.remove()
+		})
 	});
 });
-	// $("#new_question_button").hide();
+
