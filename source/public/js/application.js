c63e3b85e85f79bd9fad00707c8d6ea92719e979
Coderica <esm1018@gmail.com>
2015-05-07 13:29:04 -0700

    lots of buttons and stuff

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+	$('#new_question').hide();
+	$("#new_question_button").on('click', function(event){
+		event.preventDefault();
+		$('#new_question').show();
+		$("#new_question_button").hide();
+		$("#create_survey_button").hide();
+	});
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	$("#new_question").on('submit', function(event){
+		event.preventDefault();
+		$("#new_question").hide();
+		$("#create_survey_button").show();
+		$("#new_question_button").show();
+		// debugger
+		$('#question_list').append("<p>ajax</p>")
+	});
 });
+	// $("#new_question_button").hide();
