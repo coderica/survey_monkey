9e7c001f95fac0164b9ecdb59541d491eb0202a8
Darwin D Wu <wuddarwin@gmail.com>
2015-05-08 15:15:42 -0700

    Finish saving custom form to db

--- source/app/controllers/index_controller.rb
+++ source/app/controllers/index_controller.rb
@@ -3,24 +3,11 @@ get '/' do
 end
 
 
-post '/short_answer' do
+post '/opinion' do
 	if request.xhr?
 		survey_question = params[:question]
 		selector_option = params[:type]
-		case selector_option
-		when "five_star" 
-			return erb :_five_star, :layout => false, locals: {question: survey_question}		
-		when "yes_no" 
-			return erb :_yes_no, :layout => false, locals: {question: survey_question}			
-		when "true_false" 
-			return erb :_true_false, :layout => false, locals: {question: survey_question}
-		when "multiple_choice"
-			return erb :_multiple_choice, :layout => false, locals: {question: survey_question}		
-		when "one_to_five"
-			return erb :_one_to_five, :layout => false, locals: {question: survey_question}
-		else
-			return erb :_short_answer, :layout => false, locals: {question: survey_question}
-		end
+		return erb :"/_#{selector_option}", :layout => false, locals: {question: survey_question}		
 	else
 		return "hello"
 	end
@@ -28,12 +15,18 @@ end
 
 
 post '/form' do
-	# form = params[:form]
-	# p form
-	form = Form.new(content: params[:form]);
-
+	form = Form.new(
+		content: params[:form],
+		title: params[:title],
+		instructions: params[:instructions],
+		# user_id: current_user.id
+	);
 	if form.save
-		p 'success'
+		status 200
+		p 'foooooooooooooooooo'
+		p 'success!!!'
+	else
+		status 422
 	end
 end
 
