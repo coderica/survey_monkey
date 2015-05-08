4d65bec8b3987bcfb95be9a18da35f6179e52ad4
Coderica <esm1018@gmail.com>
2015-05-07 22:07:30 -0700

    completed survey creation with survey question options

--- source/app/controllers/index_controller.rb
+++ source/app/controllers/index_controller.rb
@@ -5,7 +5,19 @@ end
 post '/short_answer' do
 	if request.xhr?
 		survey_question = params[:question]
-		return erb :_short_answer, :layout => false, locals: {question: survey_question}
+		selector_option = params[:type]
+		case selector_option
+		when "five_star" 
+			return erb :_five_star, :layout => false, locals: {question: survey_question}		
+		when "yes_no" 
+			return erb :_yes_no, :layout => false, locals: {question: survey_question}			
+		when "true_false" 
+			return erb :_true_false, :layout => false, locals: {question: survey_question}
+		when "multiple_choice"
+			return erb :_multiple_choice, :layout => false, locals: {question: survey_question}		
+		else
+			return erb :_short_answer, :layout => false, locals: {question: survey_question}
+		end
 	else
 		return "hello"
 	end
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -10,14 +10,18 @@ $(document).ready(function() {
 
 	$("#new_question_form").on('submit', function(event){
 		event.preventDefault();
+		$("#remove_answer_button").hide();
 		$("#new_question_form").hide();
 		$("#create_new_question_button").show();
+		
 		var $surveyQuestion = $(this).find("input[type='text']")
+		var $option = $(this).find("select").val()
+
 
 		var ajax = $.ajax({
 			url: '/short_answer',
 			type: 'POST',
-			data: {question: $surveyQuestion.val()}
+			data: {question: $surveyQuestion.val(), type: $option}
 		});
 
 		ajax.done(function(response) {
@@ -25,12 +29,34 @@ $(document).ready(function() {
 			$surveyQuestion.val("")
 		});
 
+
+
 		$("#question_list").on("click", "#delete_question_button", function(event) {
 			event.preventDefault();
 			var $question = $(this).closest(".partial_question")
-			debugger
+	
 			$question.remove()
 		})
 	});
+
+
+ 	$('#question_list').on('click', "#add_answer_button", function(event){
+		event.preventDefault();
+		// debugger	
+		$('#remove_answer_button').css('display', 'inline')
+		var $list = $("#multiple_choice_list").find("li").last().clone()
+		var $add_button = $(this)
+		$add_button.hide()
+		$('#multiple_choice_list').append($list);
+	});
+
+ 	$('#question_list').on('click', "#remove_answer_button", function(event){
+		event.preventDefault();
+		var $parent_add_button = $(this).parent().prev().find("#add_answer_button")
+		$parent_add_button.css("display", "inline")
+		$(this).closest("li").remove()
+		// var $list = $($("#multiple_choice_list").find("li")[-1]).clone()
+		// $('#multiple_choice_list').append($list);
+	})
 });
 
