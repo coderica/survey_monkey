79ff917d5d1499f66c8a79f73710f365c3c2a378
Coderica <esm1018@gmail.com>
2015-05-07 23:07:13 -0700

    html/css edits

--- source/app/controllers/index_controller.rb
+++ source/app/controllers/index_controller.rb
@@ -15,6 +15,8 @@ post '/short_answer' do
 			return erb :_true_false, :layout => false, locals: {question: survey_question}
 		when "multiple_choice"
 			return erb :_multiple_choice, :layout => false, locals: {question: survey_question}		
+		when "one_to_five"
+			return erb :_one_to_five, :layout => false, locals: {question: survey_question}
 		else
 			return erb :_short_answer, :layout => false, locals: {question: survey_question}
 		end
